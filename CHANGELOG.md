32c75d8e6c6f26c326904cf2f0eaa8ee0fdaa4b6
Ronan Giron <ElGigi@users.noreply.github.com>
2024-11-28 12:46:29 +0100

    New `BerliozCommandJobHandler` handler to execute Berlioz command New `BerliozSystemJobHandler` handler to execute shell command

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file. This projec
 to [Semantic Versioning] (http://semver.org/). For change log format,
 use [Keep a Changelog] (http://keepachangelog.com/).
 
+## [2.0.0-beta2] - 2024-11-28
+
+### Added
+
+- New `BerliozCommandJobHandler` handler to execute Berlioz command
+- New `BerliozSystemJobHandler` handler to execute shell command
+
 ## [2.0.0-beta1] - 2024-11-25
 
 Start development.
---
+++ tests/Handler/BerliozSystemJobHandlerTest.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * This file is part of Berlioz framework.
+ *
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @copyright 2024 Ronan GIRON
+ * @author    Ronan GIRON <https://github.com/ElGigi>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code, to the root.
+ */
+
+namespace Berlioz\Package\QueueManager\Tests\Handler;
+
+use Berlioz\Core\Core;
+use Berlioz\Package\QueueManager\Handler\BerliozSystemJobHandler;
+use Berlioz\Package\QueueManager\TestProject\TestEnvDirectories;
+use Berlioz\Package\QueueManager\Tests\FakeJob;
+use PHPUnit\Framework\TestCase;
+
+class BerliozSystemJobHandlerTest extends TestCase
+{
+    public function testHandle()
+    {
+        $core = new Core(new TestEnvDirectories(), false);
+        $handler = new class($core) extends BerliozSystemJobHandler {
+            protected function result(false|string $output, int $result): void
+            {
+                print $output;
+                print getcwd();
+            }
+        };
+
+        ob_start();
+        $handler->handle(new FakeJob(
+            'foo',
+            'berlioz:system',
+            0,
+            ['command' => ['echo "foo bar"']],
+        ));
+        $output = ob_get_clean();
+
+        $this->assertEquals('"foo bar"' . PHP_EOL . $core->getDirectories()->getAppDir(), $output);
+    }
+}
